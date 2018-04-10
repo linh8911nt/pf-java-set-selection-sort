e49afc6c9471a1aed79d8fd8ee534a9fbc6de4f9
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-10 10:48:07 +0700

    Set selection sort algorithm

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,22 @@
 public class SelectionSort {
+    public SelectionSort() {
+    }
 
+    public void selectionSort(double[] list){
+        for (int i = 0; i < list.length; i++) {
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]){
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i){
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
 }
--- SelectionSortTest.java
+++ SelectionSortTest.java
@@ -1,2 +1,19 @@
 public class SelectionSortTest {
+    public static void main(String[] args) {
+        SelectionSort listSort = new SelectionSort();
+        double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+        System.out.println("array befor sort: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(" " + list[i]);
+        }
+        System.out.println();
+
+        listSort.selectionSort(list);
+
+        System.out.println("array after sort: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(" " + list[i]);
+        }
+    }
 }
